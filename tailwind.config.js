f46d806070eef6c2785dbf5e06ffa2ffe3419e77
skyheat <6574274+skyheat@users.noreply.github.com>
2022-11-23 01:19:51 +0800

    Tailwindcss implementation

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/*.{js,ts,jsx,tsx}',
+  './Components/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
