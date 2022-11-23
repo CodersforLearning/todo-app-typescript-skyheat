194054347bfca2532fc81af76857dde1c154d8ec
skyheat <6574274+skyheat@users.noreply.github.com>
2022-11-24 01:29:28 +0800

    Changed task and button css

--- Components/TodoTask.tsx
+++ Components/TodoTask.tsx
@@ -11,10 +11,10 @@ const TodoTask = ({ task, completeTask }: Props) => {
   return (
     <div className='w-[500px] h-[50px] flex text-black m-15px mt-2'>
         <div className='flex-[80%] h-full flex justify-center items-center'>
-            <span className='grid place-items-center w-full h-full text-lg bg-neutral-500'>{task.taskName}</span>
-            <span className='grid place-items-center w-full h-full text-lg bg-neutral-500'>{task.deadline}</span>
+            <span className='grid place-items-center w-full h-full text-lg bg-neutral-200'>{task.taskName}</span>
+            <span className='grid place-items-center w-full h-full text-lg bg-neutral-200'>{task.deadline}</span>
         </div>
-        <button className='flex-[20%] h-full bg-neutral-200 hover:bg-lime-500 cursor-pointer' onClick={() => {completeTask(task.taskName)}}>X</button>
+        <button className='flex-[20%] h-full bg-red-500 hover:text-white hover:bg-red-600 cursor-pointer' onClick={() => {completeTask(task.taskName)}}>X</button>
     </div>
   )
 }
