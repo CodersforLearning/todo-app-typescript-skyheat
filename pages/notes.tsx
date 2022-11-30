c393637cb05281cc57ad479a728d56e6b6eb9eb0
skyheat <6574274+skyheat@users.noreply.github.com>
2022-12-01 00:23:07 +0800

    Navbar work

--- pages/notes.tsx
+++ pages/notes.tsx
@@ -59,26 +59,26 @@ const Notes: FC = () => {
   }
 
   return (
-  
       <div className='flex items-center flex-col w-screen h-screen p-8 bg-cultured'>
-            <div /*className='items-start'*/>
-                <Title title={title} isEditable={isEditable} handleTitle={handleTitle} handleTitleChange={handleTitleChange}></Title>
-            </div>
-            <div>
-                    {todoList.map((task: TaskNotes, key: number) =>{
-                        return (
-                            <TodoTaskNotes key={key} task={task} index={key+1} markTask={markTask} completeTask={completeTask}/>          
-                        )
-                    })}
-                <div className='flex w-[500px] h-[50px] m-2 border-b-4'>
-                    <input className='flex-[80%] bg-cultured appearance-none outline-none h-[40px] border-none pl-2.5' type="text" placeholder="Task..." name="task" value={task} onChange={handleChange}/>
-                    {/*<button className='flex-[20%] h-[40px] border-none cursor-pointer bg-strong-cyan hover:text-cultured' onClick={addTask}>Add Task</button>*/}
-                    <PlusIcon className='transition-all ease-in-out delay-250 cursor-pointer stroke-1 hover:stroke-2' onClick={addTask}/>
-                </div>
-                <div className='flex justify-center mt-4'>
-                    {todoList.length > 0 && <ClearAll completeAllTasks={completeAllTasks}/>}
-                </div>
-            </div>
+          <NavBar navBarOpen={navBarOpen} handleNavBar={handleNavBar}/>
+          <div className='items-start'>
+              <Title title={title} isEditable={isEditable} handleTitle={handleTitle} handleTitleChange={handleTitleChange}></Title>
+          </div>
+          <div>
+              {todoList.map((task: TaskNotes, key: number) =>{
+                  return (
+                    <TodoTaskNotes key={key} task={task} index={key+1} markTask={markTask} completeTask={completeTask}/>          
+                  )
+              })}
+              <div className='flex w-[500px] h-[50px] m-2 border-b-4'>
+                  <input className='flex-[80%] bg-cultured appearance-none outline-none h-[40px] border-none pl-2.5' type="text" placeholder="Task..." name="task" value={task} onChange={handleChange}/>
+                  {/*<button className='flex-[20%] h-[40px] border-none cursor-pointer bg-strong-cyan hover:text-cultured' onClick={addTask}>Add Task</button>*/}
+                  <PlusIcon className='transition-all ease-in-out delay-250 cursor-pointer stroke-1 hover:stroke-2' onClick={addTask}/>
+              </div>
+              <div className='flex justify-center mt-4'>
+                  {todoList.length > 0 && <ClearAll completeAllTasks={completeAllTasks}/>}
+              </div>
+          </div>
         </div>
   )
 }
